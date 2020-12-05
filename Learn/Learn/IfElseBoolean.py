f4412d95ee648ca68cb9af4f8adbb82db6de901f
Ilya <none>
2020-12-05 19:16:28 +0300

    ASPDKP;ODAS

--- Learn/Learn/IfElseBoolean.py
+++ Learn/Learn/IfElseBoolean.py
@@ -126,7 +126,7 @@ for i in range (1,100):
 
 elapsed_time = timeit.timeit(code_to_test, number=100)/100
 print(elapsed_time)
-
+laDJLKJLJ
 
 # vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
 print('---------------------------------------------------------------------------------------------')
